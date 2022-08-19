d1c320434bd52ff280d6cbdf9c4378c1eea4d026
kouse-ooshiro <okinan2163@gmail.com>
2022-08-20 05:01:24 +0900

    お試しコミット

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-// import { useHistory } from "react-router-dom";
-
-// import GlobalNav from "../components/pages/GlobalNav";
 import TopPage from "../components/pages/TopPage";
-// import Register from "../components/pages/Register";
 import LoginPage from "../components/pages/LoginPage";
 import RegisterPage from "../components/pages/auth/RegisterPage";
 import AlertPage from "../components/pages/AlertPage";
