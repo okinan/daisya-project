d9761392514cdb6d3ff7961352cd378b73aee17f
kouse-ooshiro <okinan2163@gmail.com>
2022-08-21 02:53:09 +0900

    車検テーブル項目調整&権限管理

--- src/components/molecules/SyakenTable.tsx
+++ src/components/molecules/SyakenTable.tsx
@@ -1,101 +1,90 @@
-import MaterialTable, { MaterialTableProps } from "@material-table/core";
-import { Icons } from "@material-table/core";
+import MaterialTable from "@material-table/core";
 import { useState } from "react";
 import { TextField } from "@mui/material";
+import { useAppSelector } from "../../app/hooks";
+import { selectAdmin } from "../../features/auth/authSlice";
 
-import AddIcon from "@mui/icons-material/Add";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
-import CheckIcon from "@mui/icons-material/Check";
-import SearchIcon from "@mui/icons-material/Search";
-import CloseIcon from "@mui/icons-material/Close";
-import React from "react";
 
-export interface TableProps<RowData extends object>
-  extends MaterialTableProps<RowData> {}
+function SyakenTable() {
 
-function SyakenTable(props: any) {
+  //Reduxから取得
+  const admin = useAppSelector(selectAdmin);
+
   //テーブルに表示したいデータ
   const [tableData, setTableData] = useState([
     {
-      COMID: "1",
-      CARID: "1",
-      CARNAME: "ラパン",
-      CARNUM: "沖縄30 Y58-04",
-      CARCOMDAY: "2022-06-18",
-    },
-    {
-      COMID: "1",
-      CARID: "2",
-      CARNAME: "レクサス",
-      CARNUM: "沖縄30 Y58-04",
-      CARCOMDAY: "2022-06-19",
-    },
-    {
-      COMID: "1",
-      CARID: "3",
-      CARNAME: "プリウス",
-      CARNUM: "沖縄30 Y58-04",
-      CARCOMDAY: "2022-06-20",
-    },
-    {
-      COMID: "1",
-      CARID: "4",
-      CARNAME: "BMW",
-      CARNUM: "沖縄30 Y58-04",
-      CARCOMDAY: "2022-06-21",
-    },
+      docId: "999999",
+      carName: "ラパン",
+      carNum: "沖縄 30 Y58-04",
+      carComDay: "2022-06-18",
+    }
   ]);
 
   //テーブルの列名を指定
   const columns = [
     {
-      title: "会社ID",
-      field: "COMID",
+      title: "ドキュメントID",
+      field: "docId",
       sorting: false,
       hidden: true,
     },
-    { title: "車両ID", field: "CARID", sorting: false, hidden: true },
     {
       title: "車種",
-      field: "CARNAME",
+      field: "carName",
       sorting: false,
       headerStyle: { whiteSpace: "nowrap" },
       cellStyle: { whiteSpace: "nowrap" },
+      validate: (rowData: any) => {
+        if (rowData.carName === undefined || rowData.carName === "") {
+          return "車種を入力してください"
+        } 
+      }
     },
     {
       title: "ナンバー",
-      field: "CARNUM",
+      field: "carNum",
       sorting: false,
       headerStyle: { whiteSpace: "nowrap" },
       cellStyle: { whiteSpace: "nowrap" },
+      validate: (rowData: any) => {
+        if (rowData.carNum === undefined || rowData.carNum === "") {
+          return "ナンバーを入力してください"
+        } 
+      }
     },
     {
       title: "車検満了日",
-      field: "CARCOMDAY",
+      field: "carComDay",
       type: "date",
+      validate: (rowData: any) => {
+        console.log(rowData.carComDay);
+        if (rowData.carComDay === undefined || rowData.carComDay === "") {
+          return "車検満了日を入力してください"
+        } 
+      },
       editComponent: (props: any) => (
         <TextField
           type="date"
+          error={props.error}
+          helperText={props.helperText}
           defaultValue={props.value}
           onChange={(e) => props.onChange(e.target.value)}
+          variant="standard"
         />
       ),
     },
   ];
 
-  return (
-    <>
-      <MaterialTable
-        columns={columns as any}
-        data={tableData}
-        editable={{
-          onRowAdd: (newRow) =>
+  const editFunc:any = (adminFlg: boolean) => {
+    if(adminFlg){
+      return (
+        {
+          onRowAdd: (newRow: any) =>
             new Promise<void>((resolve, reject) => {
               setTableData([...tableData, newRow]);
               setTimeout(() => resolve(), 500);
             }),
-          onRowUpdate: (newRow, oldRow: any) =>
+          onRowUpdate: (newRow: any, oldRow: any) =>
             new Promise<void>((resolve, reject) => {
               const updatedData = [...tableData];
               updatedData[oldRow.tableData.id] = newRow;
@@ -109,46 +98,69 @@ function SyakenTable(props: any) {
               setTableData(updatedData);
               setTimeout(() => resolve(), 500);
             }),
-        }}
-        localization={{
-          error: "エラー",
-          header: {
-            actions: "編集",
+        }
+      );
+    }
+    return {};
+  }
+
+  const localFunc: any = () => {
+    return (
+      {
+        error: "エラー",
+        header: {
+          actions: "編集",
+        },
+        body: {
+          emptyDataSourceMessage: "対象のデータが存在しません。",
+          filterRow: {
+            filterPlaceHolder: "",
+            filterTooltip: "フィルター",
           },
-          body: {
-            emptyDataSourceMessage: "対象のデータが存在しません。",
-            filterRow: {
-              filterPlaceHolder: "",
-              filterTooltip: "フィルター",
-            },
-            editRow: {
-              saveTooltip: "保存",
-              cancelTooltip: "キャンセル",
-              deleteText: "この行を削除しますか？",
-            },
-            addTooltip: "追加",
-            deleteTooltip: "削除",
-            editTooltip: "編集",
+          editRow: {
+            saveTooltip: "保存",
+            cancelTooltip: "キャンセル",
+            deleteText: "この行を削除しますか？",
           },
-          toolbar: {
-            searchTooltip: "検索",
-            searchPlaceholder: "検索",
-            searchAriaLabel: "検索",
-            clearSearchAriaLabel: "クリア",
-          },
-        }}
-        options={{
-          sorting: true,
-          paging: false,
-          addRowPosition: "first",
-          searchFieldAlignment: "left",
-          actionsColumnIndex: 4,
-          showTitle: false,
-          rowStyle: (rowData, index) => ({
-            backgroundColor: index % 2 === 0 ? "#fdfcfb" : "#fff",
-          }),
-          headerStyle: { background: "#fff6e6" },
-        }}
+          addTooltip: "追加",
+          deleteTooltip: "削除",
+          editTooltip: "編集",
+        },
+        toolbar: {
+          searchTooltip: "検索",
+          searchPlaceholder: "検索",
+          searchAriaLabel: "検索",
+          clearSearchAriaLabel: "クリア",
+        },
+      }
+    )
+  }
+
+  const optionFunc: any = () => {
+    return (
+      {
+        sorting: true,
+        paging: false,
+        addRowPosition: "first",
+        searchFieldAlignment: "left",
+        actionsColumnIndex: 4,
+        showTitle: false,
+        rowStyle: (rowData: any, index: number) => ({
+          backgroundColor: index % 2 === 0 ? "#fdfcfb" : "#fff",
+        }),
+        headerStyle: { background: "#fff6e6" },
+      }
+    )
+  }
+
+  return (
+    <>
+      <MaterialTable
+        columns={columns as any}
+        data={tableData}
+        editable={editFunc(admin)}
+        localization={localFunc()}
+        options={optionFunc()}
         title="車検一覧"
       />
     </>
